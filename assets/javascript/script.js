5a3dfda5ebcf9fa4b7d7ed14e0da4af8f7bad9f4
Shaheshang24 <thomas.cortes24@gmail.com>
2021-04-17 14:02:23 +0200

    Added bases of navbar

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+let burgerMenu = document.getElementById("burger");
+let topElement = document.getElementById("spantop");
+let centerElement = document.getElementById("spancenter");
+let botElement = document.getElementById("spanbot");
+let menuElements = document.getElementById("nav");
+
+burgerMenu.addEventListener("click", () => {
+  topElement.classList.toggle("spantopclose");
+  centerElement.classList.toggle("spancenterclose");
+  botElement.classList.toggle("spanbotclose");
+  topElement.classList.toggle("spantopopen");
+  centerElement.classList.toggle("spancenteropen");
+  botElement.classList.toggle("spanbotopen");
+  menuElements.classList.toggle("menuopen");
+});
