bb450c72cab059cb76213a000a88238ceaa29a5d
Shaheshang24 <thomas.cortes24@gmail.com>
2021-04-20 15:39:05 +0200

    Footer Done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,9 @@ let topElement = document.getElementById("spantop");
 let centerElement = document.getElementById("spancenter");
 let botElement = document.getElementById("spanbot");
 let menuElements = document.getElementById("nav");
+let homeButton = document.getElementById("homebutton");
+let homeMenu = document.getElementById("homemenu");
+console.log(homeMenu);
 
 burgerMenu.addEventListener("click", () => {
   topElement.classList.toggle("spantopclose");
@@ -12,4 +15,9 @@ burgerMenu.addEventListener("click", () => {
   centerElement.classList.toggle("spancenteropen");
   botElement.classList.toggle("spanbotopen");
   menuElements.classList.toggle("menuopen");
+});
+
+homeButton.addEventListener("click", () => {
+  console.log("ok");
+  homeMenu.classList.toggle("homeon");
 });
