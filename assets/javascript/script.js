352183718ddc44b4823d316aa1f72e9274b62c74
Anne-Claire <ac.coutoux@gmail.com>
2021-04-20 15:33:07 +0200

    ajout slider product

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,58 @@
+// Slider product
+var slidesp = document.querySelectorAll(".slide-product");
+var btnsp = document.querySelectorAll(".btn-product");
+let currentProduct = 1;
+
+// JavaScript for slider manual navigation
+var manualNav1 = function (manual1) {
+  slidesp.forEach((slidep) => {
+    slidep.classList.remove("actives");
+
+    btnsp.forEach((btnp) => {
+      btnp.classList.remove("actives");
+    });
+  });
+  slidesp[manual1].classList.add("actives");
+  btnsp[manual1].classList.add("actives");
+};
+
+btnsp.forEach((btnp, i) => {
+  btnp.addEventListener("click", () => {
+    manualNav1(i);
+    currentProduct = i;
+  });
+});
+
+// JavaScript for slider automatic navigation
+var repeat = function(activeClass) {
+    let actives = document.getElementsByClassName('actives');
+    let i = 1;
+
+    var repeater = () => {
+        setTimeout(function(){
+            [...actives].forEach((activesProduct) => {
+                activesProduct.classList.remove('actives');
+            })
+
+            slidesp[i].classList.add('actives');
+            btnsp[i].classList.add('actives');
+            i++;
+
+            if(slidesp.length == i){
+                i = 0;
+            }
+            if(i >= slidesp.length){
+                return;
+            }
+            repeater();
+        }, 5000);
+    }
+    repeater();
+}
+repeat();
+
+
+
 // Slider recipes
 var slides = document.querySelectorAll(".slide");
 var btns = document.querySelectorAll(".btn");
